342e14ccb029493e613d295ba010f32a4d9e8947
George Clinkscales <1lclink2@att.net>
2026-04-22 20:41:09 -0400

    [Feature] Added script inclusion and navigation highlighting
    - Added `<script src="js/main.js"></script>` to multiple HTML files to include a common JavaScript file.
    - Implemented navigation highlighting in `js/main.js` to add the `active` class to the current page's navigation link.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ window.addEventListener('click', function(e) {
         e.target.classList.remove('active');
     }
 });
+
+const currentPage = window.location.pathname.split('/').pop()
+const navLinks = document.querySelectorAll('nav ul a');
+
+navLinks.forEach(link => {
+    const linkPage = link.getAttribute('href').split('/').pop();
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
+});
